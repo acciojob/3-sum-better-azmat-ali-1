3fbf6639a14de5447e1c616d6a05caa5ae65771e
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-13 00:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let i=0;
+	let ans=0;
+	while(i<arr.length){
+	let j=i+2;
+	while(i+1<arr.length&&j<arr.length){
+		let total = arr[i]+arr[i+1]+arr[j];
+		if(total-target<sum && target<total){
+			sum=total;
+		}
+		if(total+target<sum && target>total){
+			sum=total;
+		}
+		j++;
+	}
+		i++;
+	}
+  return sum;
 }
 
 module.exports = threeSum;
